ad1234de269f4bdc73005214493e72aebd146399
Raymond Lowe <raymond.lowe@gov.bc.ca>
2022-03-14 13:09:38 -0700

    reset

---
+++ react-sandbox/src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+//Pages
+import Home from "../src/pages/home"
+import About from "../src/pages/about"
+import Users from "../src/pages/users"
+import Modal from "../src/pages/modal"
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/modal">Modal</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/modal" element={<Modal/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App
